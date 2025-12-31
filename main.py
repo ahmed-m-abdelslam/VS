298f3f9dce5748e7187cf378a74c25140684b8b8
ahmed-m-abdelslam <amjj6367@gmail.com>
2025-12-31 03:54:36 +0200

    Implement base router with welcome endpoint and update requirements

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+from routes import base
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to the FastAPI application!"}
-
-
-g
+app.include_router(base.base_router)
