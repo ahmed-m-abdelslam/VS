ccf185c89525707244ae8a0b3dce6f68a75ec5ac
ahmed-m-abdelslam <amjj6367@gmail.com>
2026-01-07 11:59:54 +0200

    Add LLMProviderFactory and update configuration settings for OpenAI and Cohere; remove unused enums and interfaces

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI # type: ignore
 from routes import base ,data
 from motor.motor_asyncio import AsyncIOMotorClient # type: ignore
 from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 app = FastAPI()
 
@@ -10,6 +11,19 @@ async def startup_db_client():
     settings = get_settings()
     app.mongodb_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongodb_conn[settings.MONGODB_DB_NAME]
+
+    llm_provider_factory = LLMProviderFactory(config=settings)
+
+    # Initialize generation client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+
+    # Initialize embedding client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(
+        model_id=settings.EMBEDDING_MODEL_ID,
+        embedding_size=settings.EMBEDDING_MODEL_SIZE
+    )
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
